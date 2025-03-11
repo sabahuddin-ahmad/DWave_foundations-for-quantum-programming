7e2290e07e83d291fee603d410b8a7dc47b8b8b6
sabahuddin-ahmad <82322721+sabahuddin-ahmad@users.noreply.github.com>
2025-03-11 17:42:30 -0600

    submission

--- Function/function_practice.py
+++ Function/function_practice.py
@@ -1,1 +1,11 @@
+def grocery(c):
+    if c == "tofu":  
+        print("Tofu is cheap and has good calories")
+    elif c == "meat":  
+        print("Meat has the most calories")
+    elif c == "salmon" or c == "white fish":  
+        print("Seafood is expensive but is healthy")
+    else:  
+        print("Chicken has fewer calories than meat")
 
+grocery("meat")
